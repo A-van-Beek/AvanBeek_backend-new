94d90b632e8707d5b354fb664225afa52d55cb52
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2023-10-18 20:45:04 +0200

    add GET / PUT / DELETE

---
+++ express-book-app/services/books/updateBookById.js
@@ -0,0 +1,22 @@
+import bookData from "../../data/books.json" assert { type: "json" };
+
+const updateBookById = (id, title, author, isbn, pages, available, genre) => {
+  const book = bookData.books.find((book) => book.id === id);
+
+  if (!book) {
+    throw new Error(`Book with id ${id} was not found!`);
+  }
+
+  book.title = title ?? book.title;
+  book.author = author ?? book.author;
+  book.isbn = isbn ?? book.isbn;
+  book.pages = pages ?? book.pages;
+  book.available = available ?? book.available;
+  book.genre = genre ?? book.genre;
+
+  return book;
+};
+// vraagtekens geven aan optioneel:
+// als geen nieuwe titel gegeven, dan book.titel gebruiken
+
+export default updateBookById;
